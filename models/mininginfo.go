b02fcf85db47d64eefc1b39fb95f0256c0f47fda
Jonathan Herrmann <jonorh@web.de>
2023-10-18 21:13:36 +0200

    Change data type of hashrate

--- models/mininginfo.go
+++ models/mininginfo.go
@@ -3,9 +3,8 @@ package models
 type MiningInfo struct {
 	Blocks                     uint64  `json:"blocks"`
 	Difficulty                 float64 `json:"difficulty"`
-	NetworkHashPerSecondString string  `json:"networkhashps"`
-	NetworkHashPerSecond       int64
-	PooledtTx                  uint64 `json:"pooledtx"`
-	Chain                      string `json:"chain"`
-	Warnings                   string `json:"warnings"`
+	NetworkHashPerSecondString float64 `json:"networkhashps"`
+	PooledtTx                  uint64  `json:"pooledtx"`
+	Chain                      string  `json:"chain"`
+	Warnings                   string  `json:"warnings"`
 }
