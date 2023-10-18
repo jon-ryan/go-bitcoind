ffc0e4b35dbe2e9f2aa8217f1d61ab7c9aa463e8
Jonathan Herrmann <jonorh@web.de>
2023-10-18 21:14:32 +0200

    Rename variable

--- models/mininginfo.go
+++ models/mininginfo.go
@@ -1,10 +1,10 @@
 package models
 
 type MiningInfo struct {
-	Blocks                     uint64  `json:"blocks"`
-	Difficulty                 float64 `json:"difficulty"`
-	NetworkHashPerSecondString float64 `json:"networkhashps"`
-	PooledtTx                  uint64  `json:"pooledtx"`
-	Chain                      string  `json:"chain"`
-	Warnings                   string  `json:"warnings"`
+	Blocks               uint64  `json:"blocks"`
+	Difficulty           float64 `json:"difficulty"`
+	NetworkHashPerSecond float64 `json:"networkhashps"`
+	PooledtTx            uint64  `json:"pooledtx"`
+	Chain                string  `json:"chain"`
+	Warnings             string  `json:"warnings"`
 }
